ef879bc0dfb5c33752b6ceda15f8af3dfc2bb269
James Odeyale <jamesodeyale01@gmail.com>
2023-05-07 17:01:21 -0400

    completed the styling for the single habit page

--- src/screen/Habit/components/SingleHabit/SingleHabit.tsx
+++ src/screen/Habit/components/SingleHabit/SingleHabit.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { CustomCalendar } from '../../../../components'
 import { APP_GRAY, APP_RED, APP_WHITE, GRAY_TEXT, HABIT_OPTION, MAIN_ACCENT_COLOR } from '../../../../styles'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { StreakIcon } from '../../../../assets/svgs'
 
 
 export const SingleHabit = ({ route, navigation }) => {
@@ -28,33 +29,32 @@ export const SingleHabit = ({ route, navigation }) => {
         <Text style={styles.habitName}>Read a book</Text>
         <Text style={styles.habitDescription}>I want to read 10 pages of a book everyday.</Text>
 
-        <View>
+        <View style={styles.habitInfo}>
           <View>
-            <Text>Repeat:</Text>
-            <Text>Daily</Text>
+            <Text style={styles.habitInfoText}>Repeat:</Text>
+            <Text style={styles.habitInfoText_Frequency}>Daily</Text>
           </View>
           <View>
-            <Text>Remind:</Text>
-            <Text>01:30 PM</Text>
+            <Text style={styles.habitInfoText}>Remind:</Text>
+            <Text style={styles.habitInfoText_Frequency}>01:30 PM</Text>
           </View>
         </View>
 
-        <View>
-          <CustomCalendar />
-        </View>
-        <View>
-          <View>
+        <CustomCalendar />
+
+        <View style={styles.streakContainer}>
+          <View style={styles.streakVSLongestStreak}>
             <View>
-              <Text>0 DAYS</Text>
-              <Text>Your Current Streak</Text>
+              <Text style={styles.streakDay}>0 DAYS</Text>
+              <Text style={styles.streakLabel}>Your Current Streak</Text>
             </View>
             <View>
-              <Text>0 days</Text>
-              <Text>Your longest streak</Text>
+              <Text style={styles.longestStreak}>0 days</Text>
+              <Text style={styles.longestStreakLabel}>Your longest streak</Text>
             </View>
           </View>
           <View>
-            <Text>Image</Text>
+            <StreakIcon />
           </View>
         </View>
         <TouchableOpacity
@@ -95,7 +95,8 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 24,
     lineHeight: 29,
-    color: GRAY_TEXT
+    color: GRAY_TEXT,
+    marginBottom: 10
   },
   habitDescription: {
     fontFamily: 'Inter',
@@ -104,6 +105,82 @@ const styles = StyleSheet.create({
     fontSize: 14,
     lineHeight: 18,
     color: GRAY_TEXT
+  },
+  habitInfo: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 25,
+    marginBottom: 25,
+    width: 200,
+    alignSelf: 'center'
+  },
+  habitInfoText: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 12,
+    lineHeight: 18,
+    color: GRAY_TEXT,
+    textAlign: 'center',
+    marginBottom: 5
+  },
+  habitInfoText_Frequency: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: 16,
+    lineHeight: 18,
+    color: GRAY_TEXT,
+    textAlign: 'center'
+  },
+  streakContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 45,
+    marginTop: 25,
+    height: 130
+  },
+  streakVSLongestStreak: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  streakDay: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 40,
+    lineHeight: 48,
+    color: MAIN_ACCENT_COLOR
+  },
+  streakLabel: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 17,
+    color: MAIN_ACCENT_COLOR,
+    opacity: 0.7
+  },
+  longestStreak: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 12,
+    lineHeight: 15,
+    color: MAIN_ACCENT_COLOR
+  },
+  longestStreakLabel: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 12,
+    lineHeight: 15,
+    color: MAIN_ACCENT_COLOR,
+    opacity: 0.7
   },
   createButton: {
     backgroundColor: MAIN_ACCENT_COLOR,
