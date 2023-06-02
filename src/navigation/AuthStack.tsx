2bd62714403f20f7481195f718b7686a339e7d0d
James Odeyale <jamesodeyale01@gmail.com>
2023-06-01 21:13:41 -0400

    add prettier

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,16 +1,19 @@
-import { ROUTES } from '../constants'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { AuthScreen, OnboardScreen, RecoveryScreen } from '../screen'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ROUTES } from '../constants';
+import { AuthScreen, OnboardScreen, RecoveryScreen } from '../screen';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
-export const AuthStack = () => {
+export function AuthStack() {
   return (
-    <Stack.Navigator initialRouteName='Onboard' screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      initialRouteName="Onboard"
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen name={ROUTES.AUTH} component={AuthScreen} />
       <Stack.Screen name={ROUTES.BENEFIT} component={OnboardScreen} />
       <Stack.Screen name={ROUTES.RECOVER_ACCOUNT} component={RecoveryScreen} />
     </Stack.Navigator>
-  )
+  );
 }
--- src/screen/Onboard/components/AuthScreen/AuthScreen.tsx
+++ src/screen/Onboard/components/AuthScreen/AuthScreen.tsx
@@ -1,35 +1,31 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
-import { SECONDARY_BG_COLOR } from '../../../../styles'
-import { LoginForm } from './components/LoginForm'
-import { SignUpForm } from './components/SignUpForm'
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { SECONDARY_BG_COLOR } from "../../../../styles";
+import { LoginForm } from "./components/LoginForm";
+import { SignUpForm } from "./components/SignUpForm";
+
+export function AuthScreen() {
+    const [formType, setFormType] = React.useState("login");
 
 
-export const AuthScreen = () => {
-  const [formType, setFormType] = React.useState('login')
+    const changeBetweenForms = () => {
+        if (formType === "login") {
+            setFormType("register");
+        }
+        if (formType === "register") {
+            setFormType("login");
+        }
+    };
 
-  const changeBetweenForms = () => {
-    if (formType === 'login') {
-      setFormType('register')
-    }
-    if (formType === 'register') {
-      setFormType('login')
-    }
-
-  }
-
-  return (
-    <SafeAreaView style={styles.AuthScreenContainer}>
-      {formType === 'login' ?
-        <LoginForm changeBetweenForms={changeBetweenForms} />
-        : <SignUpForm changeBetweenForms={changeBetweenForms} />}
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView style={styles.AuthScreenContainer}>
+            {formType === "login" ? <LoginForm changeBetweenForms={changeBetweenForms} /> : <SignUpForm changeBetweenForms={changeBetweenForms} />}
+        </SafeAreaView>
+    );
 }
 
-
 const styles = StyleSheet.create({
-  AuthScreenContainer: {
-    backgroundColor: SECONDARY_BG_COLOR
-  }
-})
+    AuthScreenContainer: {
+        backgroundColor: SECONDARY_BG_COLOR,
+    },
+});
