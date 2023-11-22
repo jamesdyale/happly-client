57efa613c171303849de333863dc3ef4130c79fb
James Odeyale <jamesodeyale01@gmail.com>
2023-11-22 18:41:16 -0500

    appstate active pulls new date and time of day

--- src/data/firebaseConfig.ts
+++ src/data/firebaseConfig.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from "@env";
 
@@ -17,4 +16,3 @@ export const FIREBASE_APP = initializeApp(firebaseConfig);
 
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-const analytics = getAnalytics(FIREBASE_APP);
--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,43 @@
 import "react-native-gesture-handler";
-import React, { useEffect, useRef } from "react";
-import { Provider as StateProvider, useAtom, useAtomValue } from "jotai";
+import React, { useEffect } from "react";
+import { Provider as StateProvider, useAtom } from "jotai";
 import { ToastProvider } from "react-native-toast-notifications";
 import { Navigation } from "~navigation";
 import { AppLoading } from "~AppLoading";
 import { AppState } from "react-native";
-import { currentTimeOfDayAtom, selectedDayOfTheWeekAtom, userAtom } from "~state";
+import { currentTimeOfDayAtom, selectedDayOfTheWeekAtom } from "~state";
 import moment from "moment";
 import { GetCurrentTimeOfDay } from "~utils/timeUtils";
 
 const App = () => {
   // TODO: Bring in NativeBaseProvider for light mode dark mode - https://github.com/kacgrzes/expo-typescript-template/blob/981dc18e5a7df5ec3b9be0fd847cd6e0a01e4da2/src/index.tsx#L32
-  const appState = useRef(AppState.currentState);
   const [selectedDay, setSelectedDay] = useAtom(selectedDayOfTheWeekAtom);
   const [currentTimeOfDay, setCurrentTimeOfDay] = useAtom(currentTimeOfDayAtom);
+  const [appState, setAppState] = React.useState(AppState.currentState);
 
   useEffect(() => {
-    const subscription = AppState.addEventListener("change", (nextAppState) => {
-      if (appState.current.match(/active/) && nextAppState === "active") {
-        // set the selected day to the current day
-        const currentDay = moment().format("MMMM Do YYYY");
-        if (selectedDay !== currentDay) {
-          setSelectedDay(currentDay);
-        }
-
-        const timeOfDay = GetCurrentTimeOfDay();
-        if (currentTimeOfDay !== timeOfDay) {
-          setCurrentTimeOfDay(timeOfDay);
-        }
-      }
-    });
+    const subscription = AppState.addEventListener("change", handleAppStateChange);
 
     return () => {
       subscription.remove();
     };
   }, []);
+
+  const handleAppStateChange = (nextAppState) => {
+    if (nextAppState === "active") {
+      // set the selected day to the current day
+      const currentDay = moment().format("MMMM Do YYYY");
+      if (selectedDay !== currentDay) {
+        setSelectedDay(currentDay);
+      }
+
+      const timeOfDay = GetCurrentTimeOfDay();
+      if (currentTimeOfDay !== timeOfDay) {
+        setCurrentTimeOfDay(timeOfDay);
+      }
+    }
+    setAppState(nextAppState);
+  };
 
   return (
     <StateProvider>
